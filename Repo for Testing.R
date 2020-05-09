9c57cff3eefeb96da635dcc9c08c31f1844799de
JimmyDang14 <Jimmy.Dang@etu.unige.ch>
2020-05-10 01:36:12 +0200

    update

--- Repo for Testing.R
+++ Repo for Testing.R
@@ -71,14 +71,16 @@ rm(x)
 
 matrixDepensesInvestiss_00_17 <- data.matrix(DepensesInvestiss_00_17)
 matrixNHabitants_00_19 <- data.matrix(NHabitants_00_19)
-matrixratioDIprov <- sweep(matrixDepensesInvestiss_00_17, 1, matrixNHabitants_00_19, "/")
+matrixNHabitants_00_19prov <- matrixNHabitants_00_19[,-(21:22)]
+matrixratioDIprov <- sweep(matrixDepensesInvestiss_00_17, 1, matrixNHabitants_00_19prov, "/")
 matrixratioDI <- matrixratioDIprov * 1000
 ratioDIprov <- as.data.frame(matrixratioDI)
 ratioDIprov[1] <- NULL
 ratioDIprov2 <- head(cbind(Commune, ratioDIprov), 46)
 ratioDI <- ratioDIprov2[,-1]
 rownames(ratioDI) <- ratioDIprov2[,1]
-rm(matrixDepensesInvestiss_00_17, matrixNHabitants_00_19,
+rm(matrixNHabitants_00_19prov,
+   matrixDepensesInvestiss_00_17, matrixNHabitants_00_19,
    matrixratioDIprov, matrixratioDI, ratioDIprov, ratioDIprov2)
 #3.2) Loyermoyen100m2 - Pas de ratio, seulement clean
 Loyermoyen100m2_06_19[1] <- NULL
@@ -86,16 +88,16 @@ Loyermoyen100m2_06_19prov <- head(cbind(Commune, Loyermoyen100m2_06_19), 46)
 Loyermoyen100m2_06_19_OK <- Loyermoyen100m2_06_19prov[,-1]
 rownames(Loyermoyen100m2_06_19_OK) <- Loyermoyen100m2_06_19prov[,1]
 rm(Loyermoyen100m2_06_19, Loyermoyen100m2_06_19prov)
-#3.3) Densité Ecole primaire (nombre d'écoles primaires moyen par km2)
+#3.3) Ratio Ecole primaire (nombre d'écoles primaires moyen par km2)
 matrixNEcolePrimaire <- data.matrix(NEcolePrimaire)
 matrixSuperficie <- data.matrix(Superficie)
-matrixdensite_ecoleprov <- sweep(matrixNEcolePrimaire, 1, matrixSuperficie, "/")
-densite_ecoleprov <- as.data.frame(matrixdensite_ecoleprov)
-densite_ecoleprov[1] <- NULL
-densite_ecoleprov2 <- head(cbind(Commune, densite_ecoleprov), 46)
-densite_ecole <- densite_ecoleprov2[,-1]
-rownames(densite_ecole) <- densite_ecoleprov2[,1]
-rm(matrixNEcolePrimaire, matrixSuperficie, matrixdensite_ecoleprov, densite_ecoleprov,densite_ecoleprov2)
+matrixratio_ecoleprov <- sweep(matrixNEcolePrimaire, 1, matrixSuperficie, "/")
+ratio_ecoleprov <- as.data.frame(matrixratio_ecoleprov)
+ratio_ecoleprov[1] <- NULL
+ratio_ecoleprov2 <- head(cbind(Commune, ratio_ecoleprov), 46)
+ratio_ecole <- ratio_ecoleprov2[,-1]
+rownames(ratio_ecole) <- ratio_ecoleprov2[,1]
+rm(matrixNEcolePrimaire, matrixSuperficie, matrixratio_ecoleprov, ratio_ecoleprov,ratio_ecoleprov2)
 #3.4) NHabitants - Pas de ratio seulement clean
 NHabitants_00_19[1] <- NULL
 NHabitantsprov <- head(cbind(Commune, NHabitants_00_19), 46)
@@ -103,9 +105,7 @@ NHabitants_OK <- NHabitantsprov[,-1]
 rownames(NHabitants_OK) <- NHabitantsprov[,1]
 rm(NHabitants_00_19, NHabitantsprov)
 #3.5) Ratio Infraction pour 1000 habitants
-NInfractions_08_19prov <- NInfractions_08_19[-1,]
-colnames(NInfractions_08_19prov) <- NInfractions_08_19[1,]
-matrixNInfractions_08_19 <- data.matrix(NInfractions_08_19prov)
+matrixNInfractions_08_19 <- data.matrix(NInfractions_08_19)
 matrixNHabitants_00_19 <- data.matrix(NHabitants_00_19)
 matrixNHabitants_00_19prov <- matrixNHabitants_00_19[,-(3:10)]
 matrixratioInfractionprov <- sweep(matrixNInfractions_08_19, 1, matrixNHabitants_00_19prov, "/")
@@ -115,33 +115,130 @@ ratioInfractionprov[1] <- NULL
 ratioInfractionprov2 <- head(cbind(Commune, ratioInfractionprov), 46)
 ratioInfraction <- ratioInfractionprov2[,-1]
 rownames(ratioInfraction) <- ratioInfractionprov2[,1]
-rm(NInfractions_08_19prov,
-   matrixNInfractions_08_19, matrixNHabitants_00_19, matrixNHabitants_00_19prov,
+rm(  matrixNInfractions_08_19, matrixNHabitants_00_19, matrixNHabitants_00_19prov,
    matrixratioInfractionprov,matrixratioInfraction,ratioInfractionprov,ratioInfractionprov2)
+#3.6) Ratio Medecin pour 1000 habitants
+matrixNMedecins <- data.matrix(NMedecins)
+matrixNHabitants_00_19 <- data.matrix(NHabitants_00_19)
+matrixNHabitants_00_19prov <- matrixNHabitants_00_19[,-(21:22)]
+matrixNMedecinsprov <- sweep(matrixNMedecins, 1, matrixNHabitants_00_19prov, "/")
+matrixratioMedecins <- matrixNMedecinsprov * 1000
+ratioMedecinsprov <- as.data.frame(matrixratioMedecins)
+ratioMedecinsprov[1] <- NULL
+ratioMedecinsprov2 <- head(cbind(Commune, ratioMedecinsprov), 46)
+ratioMedecins <- ratioMedecinsprov2[,-1]
+rownames(ratioMedecins) <- ratioMedecinsprov2[,1]
+rm(  matrixNMedecins, matrixNHabitants_00_19 ,matrixNHabitants_00_19prov,matrixNMedecinsprov,matrixratioMedecins,ratioMedecinsprov ,ratioMedecinsprov2 )
+
+#3.7) Ratio Parcs par km2
+matrixNParcs <- data.matrix(NParcs)
+matrixSuperficie <- data.matrix(Superficie)
+matrixratio_NParcsprov <- sweep(matrixNParcs, 1, matrixSuperficie, "/")
+ratio_NParcsprov <- as.data.frame(matrixratio_NParcsprov)
+ratio_NParcsprov[1] <- NULL
+ratio_NParcsprov2 <- head(cbind(Commune, ratio_NParcsprov), 46)
+ratio_NParcs <- ratio_NParcsprov2[,-1]
+rownames(ratio_ecole) <- ratio_NParcsprov2[,1]
+rm(matrixNParcs, matrixSuperficie, matrixratio_NParcsprov, ratio_NParcsprov,ratio_NParcsprov2)
+
+#3.8) Ratio Supermarches par km2
+matrixNSupermarches <- data.matrix(NSupermarches)
+matrixNSupermarchesprov <- matrixNSupermarches[,-(4:9)]
+matrixSuperficie <- data.matrix(Superficie)
+matrixratio_NSupermarchesprov <- sweep(matrixNSupermarchesprov, 1, matrixSuperficie, "/")
+ratio_NSupermarchesprov <- as.data.frame(matrixratio_NSupermarchesprov)
+ratio_NSupermarchesprov[1] <- NULL
+ratio_NSupermarchesprov2 <- head(cbind(Commune, ratio_NSupermarchesprov), 46)
+ratio_Supermarches <- ratio_NSupermarchesprov2[,-1]
+rownames(ratio_Supermarches) <- ratio_NSupermarchesprov2[,1]
+rm(matrixNSupermarches, matrixNSupermarchesprov,matrixSuperficie, matrixratio_NSupermarchesprov, ratio_NSupermarchesprov,ratio_NSupermarchesprov2)
+
+#3.9) Prix médiane appartement 100m2 - Pas de ratio, seulement clean
+PMedianeAppart100m2_00_17[1] <- NULL
+PMedianeAppart100m2_00_17prov <- head(cbind(Commune, PMedianeAppart100m2_00_17), 46)
+PMedianeAppart100m2_00_17_OK <- PMedianeAppart100m2_00_17prov[,-1]
+rownames(PMedianeAppart100m2_00_17_OK) <- PMedianeAppart100m2_00_17prov[,1]
+rm(PMedianeAppart100m2_00_17, PMedianeAppart100m2_00_17prov)
+
+#3.10) Prix médiane Villa - Pas de ratio, seulement clean - Aucun détail sur dimensions du bien
+PMedianeVilla_00_17[1] <- NULL
+PMedianeVilla_00_17prov <- head(cbind(Commune, PMedianeVilla_00_17), 46)
+PMedianeVilla_00_17_OK <- PMedianeVilla_00_17prov[,-1]
+rownames(PMedianeVilla_00_17_OK) <- PMedianeVilla_00_17prov[,1]
+rm(PMedianeVilla_00_17, PMedianeVilla_00_17prov)
+
+#3.11) Densité de population
+matrixNHabitants_00_19 <- data.matrix(NHabitants_00_19)
+matrixSuperficie <- data.matrix(Superficie)
+matrixSuperficieprov <- matrixSuperficie[,-(1:2)]
+matrixDensite_population <- sweep(matrixNHabitants_00_19, 1, matrixSuperficieprov, "/")
+Densite_populationprov <- as.data.frame(matrixDensite_population)
+Densite_populationprov[1] <- NULL
+Densite_populationprov2 <- head(cbind(Commune, Densite_populationprov), 46)
+Densite_population <- Densite_populationprov2[,-1]
+rownames(Densite_population) <- Densite_populationprov2[,1]
+rm(matrixSuperficieprov,
+   matrixSuperficie, matrixNHabitants_00_19,
+   matrixDensite_population, Densite_populationprov, Densite_populationprov2)
+
+
 
 ###4.0) FONCTIONS PLOT------------------------------
 #4.1) DI (funratioDIplot("nom de la commune")) & examples
-
 funratioDIplot <- function(a){x <- ratioDI[c(a),c(2:19)]
 tx <- t(x)
 plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="CHF",main="Dépense moyenne d'Investissement par habitant")
 axis(1, at=1:18, labels=2000:2017)}
-
-funratioDIplot("Aire-la-Ville")
-funratioDIplot("Choulex")
 funratioDIplot("Genève")
-#4.2) Loyermoyen100m2 (funloyermoyen100m2("nom de la commune"))
-funloyermoyen100m2 <- function(a){x <- Loyermoyen100m2_06_19_OK[c(a),c(2:15)]
+
+#4.2) Loyermoyen100m2 (funloyermoyen100m2plot("nom de la commune"))
+funloyermoyen100m2plot <- function(a){x <- Loyermoyen100m2_06_19_OK[c(a),c(2:15)]
 tx <- t(x)
 plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="CHF", ylim = c(1000,2500), main="Loyer moyen appartement 100m2")
 axis(1, at=1:14, labels=2006:2019)}
+funloyermoyen100m2plot("Genève")
 
-funloyermoyen100m2("Genève")
-#4.3) Pas besoin pour école car pas de variation sur le temps ( pas d'info du moins)
-#4.4) NHabitants
-funNHabitants <- function(a){x <- NHabitants_OK[c(a),c(2:15)]
+#4.3) Pas besoin pour école car ni la superficie ni le nombre d'école varie (malheureusement, pour cette dernière variable, j'ai pas les dates d'entrées)
+#4.4) Population
+funPopulationplot <- function(a){x <- NHabitants_OK[c(a),c(2:21)]
 tx <- t(x)
 plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="Nombre d'habitants", main="Population")
 axis(1, at=1:20, labels=2000:2019)}
+funPopulationplot("Genève")
+#4.5) Ratio Infraction pour 1000 habitants
 
-funNHabitants("Genève")
+funratioInfractionplot <- function(a){x <- ratioInfraction[c(a),c(2:13)]
+tx <- t(x)
+plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="Nombre de crimes",main="Nombre d'infractions du Code Pénal par 1000 habitants")
+axis(1, at=1:12, labels=2008:2019)}
+funratioInfractionplot("Genève")
+
+#4.6) Pas besoin pour médecin parce que j'ai pas les dates d'entrée en service des médecins, du coup un plot serait questionnable pour voir l'évolution
+
+#4.7) Pas besoin pour les parcs, comme pour école
+
+#4.8) Pas besoin pour les supermarchés comme pour école
+
+#4.9) Prix médiane appartement 100m2 
+funprixmedianeappartement100m2plot <- function(a){x <- PMedianeAppart100m2_00_17_OK[c(a),c(2:19)]
+tx <- t(x)
+plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="CHF", main="Prix médiane d'un appartement 100m2")
+axis(1, at=1:18, labels=2000:2017)}
+funprixmedianeappartement100m2plot("Genève")
+
+#4.10) Prix médiane Villa
+funprixmedianevillaplot <- function(a){x <- PMedianeVilla_00_17_OK[c(a),c(2:19)]
+tx <- t(x)
+plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="CHF", main="Prix médiane d'une villa (taille inconnue)")
+axis(1, at=1:18, labels=2000:2017)}
+funprixmedianevillaplot("Genève")
+
+#4.11) Densite population (plot discutable vu que c'est la même courbe que celle de la population)
+fundensitepopulationplot <- function(a){x <- Densite_population[c(a),c(2:21)]
+tx <- t(x)
+plot( tx,type="l", col="blue", lwd=2, xlab="Year", xaxt = "n", ylab="Nombre de personnes/km2",main="Densité de la population")
+axis(1, at=1:20, labels=2000:2019)}
+fundensitepopulationplot("Genève")
+
+##5) CLEAN USELESS DATA------------------------------------------------------
+rm(Commune, DepensesInvestiss_00_17,NEcolePrimaire, NInfractions_08_19, NMedecins, NParcs, NSupermarches)
